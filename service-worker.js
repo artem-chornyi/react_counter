edf6df561cdb6fd0889e6fab9752ff6bbbab007d
Artem <temaqwerty15@gmail.com>
2021-07-26 12:08:00 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.35369277eabbfb9f41d8e1c2d600fc7c.js"
+  "./precache-manifest.3ea8619540f5c956ba1bd1cca0bf8de2.js"
 );
 
 self.addEventListener('message', (event) => {
